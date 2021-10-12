12ff2e892d81be9bde8d9603723e1007390595e9
Mihir Chaturvedi <plibither8@gmail.com>
2021-10-12 12:58:39 +0530

    feat: Add basic API structure

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,17 @@
-console.log("Hello, world!");
+import "dotenv/config";
+import express from "express";
+import { json } from "body-parser";
+
+const app = express();
+
+app.use(json());
+
+app.get("/", (req, res, next) => {
+  res.json({
+    hello: "world",
+  });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("Listening on port", process.env.PORT);
+});
