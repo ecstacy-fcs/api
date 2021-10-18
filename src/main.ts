105c31c97c44b074c9b8768417a6f9b1d341c96b
thesantatitan <dev19034@iiitd.ac.in>
2021-10-18 15:37:59 +0530

    Add basic express-session middleware

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,33 @@ import express from "express";
 import { json } from "body-parser";
 import prisma from "./common/client";
 import auth from "./routes/auth";
-
+import session from "express-session";
+import { PrismaSessionStore } from "@quixo3/prisma-session-store";
 // const prisma = new PrismaClient();
 
 const app = express();
 
 app.use(json());
+
+app.use(session({
+  secret:process.env.COOKIE_SECRET.split(' '),
+  cookie:{
+    maxAge:10*1000,
+    httpOnly:true,
+    //TODO: set secure to true,
+    secure:false,
+  },
+  name:'svetlana',
+  resave:true,
+  saveUninitialized:false,
+  store: new PrismaSessionStore(
+    prisma,
+    {
+      checkPeriod: 7*24*60*60*1000,
+      dbRecordIdIsSessionId: true,
+    }
+  )
+}));
 
 app.use("/auth", auth);
 
