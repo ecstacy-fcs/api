8ffa055b4f5feea260f9f65838a3482f3b2c48b9
thesantatitan <dev19034@iiitd.ac.in>
2021-11-09 03:55:32 +0530

    Minor bug fix

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ import search from "./routes/search";
 import sell from "./routes/sell";
 import seller from "./routes/sellers";
 import user from "./routes/user";
-
+import buyer from "./routes/buyers";
 const app = express();
 
 const parseJSON = json({ limit: "1mb" });
